18b8576f0b66688caddaef68aa6004b9bb97230c
Mark Cantrill <mark@astro-designs.com>
2019-03-03 15:02:59 +0000

    Adding text_scroll.py with support for custom message through arguments

--- sense_hat/text_scroll.py
+++ sense_hat/text_scroll.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 from sense_hat import SenseHat
+import sys
 
 sense = SenseHat()
 sense.set_rotation(180)
 red = (255, 0, 0)
-sense.show_message("One small step for Pi!", text_colour=red)
+green = (0,255,0)
+blue = (0,0,255)
+
+if len(sys.argv) > 1:
+    for x in range (1, len(sys.argv)):
+        sense.show_message(sys.argv[x])
+else:
+    sense.show_message("What shall we do today?", text_colour=blue)
